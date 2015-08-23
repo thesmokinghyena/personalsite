5b7f85b580d11684a472036d1b10a6b3df03d712
Josh Bonnell <thesmokinghyena@gmail.com>
2015-08-23 11:04:45 -0500

    Adding basic user model and secure password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822235032) do
+ActiveRecord::Schema.define(version: 20150823155411) do
 
   create_table "guestbooks", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20150822235032) do
     t.datetime "updated_at", null: false
     t.text     "title"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "visitors", force: :cascade do |t|
     t.string   "name"
